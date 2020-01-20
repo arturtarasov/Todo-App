46f31951b49054987b8f5dc481b1479ed5aacbcb
arturtarasov <artur-tarasov@mail.ru>
2020-01-20 17:30:45 +0300

    change Screens

--- App.js
+++ App.js
@@ -1,34 +1,62 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import { Navbar } from './src/Navbar'
-import { AddTodo } from './src/AddTodo'
-import { Todo } from './src/Todo'
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Navbar } from './src/components/Navbar';
+import { MainScreen } from './src/screens/MainScreen';
+import { TodoScreen } from './src/screens/TodoScreen';
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todoId, setTodoId] = useState("2");
+  const [todos, setTodos] = useState([
+    {
+      id: "1",
+      title: "выучить react native"
+    },
+    {
+      id: "2",
+      title: "написать приложение"
+    }
+  ]);
 
   const addTodo = title => {
-    setTodos(prev => [...prev, {
-      id: Date.now().toString(),
-      title,
-    }])
-  }
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ]);
+  };
 
   const removeTodo = id => {
-    setTodos(prev => prev.filter(todo => todo.id !== id))
+    setTodos(prev => prev.filter(todo => todo.id !== id));
+  };
+
+  let conent = (
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
+      openTodo={setTodoId}
+    />
+  );
+
+  if (todoId) {
+    const selectedTodo = todos.find(todo => todo.id === todoId);
+    conent = (
+      <TodoScreen
+        goBack={() => {
+          setTodoId(null);
+        }}
+        todo={selectedTodo}
+      />
+    );
   }
 
   return (
     <View>
       <Navbar title="Todo App" />
-      <View style={styles.container}>
-        <AddTodo onSubmit={ addTodo } />
-        <FlatList
-          keyExtractor={item => item.id.toString()}
-          data={todos}
-          renderItem={({item}) => <Todo todo={item} onRemove={removeTodo} />}
-        />
-      </View>
+      <View style={styles.container}>{conent}</View>
     </View>
   );
 }
---
+++ src/screens/TodoScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import { THEME } from '../theme';
+
+export const TodoScreen = ({ goBack, todo }) => {
+  return (
+    <View>
+      <Text>{todo.title}</Text>
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title="Назад" color={THEME.GREY_COLOR} onPress={goBack} />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Удалить"
+            color={THEME.DANGER_COLOR}
+            onPress={() => {
+              console.log("remove");
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  button: {
+    width: "40%"
+  }
+});
